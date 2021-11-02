91938212bbd6b4f1c739bd5256ded14af8d2cfe9
bx2k <BillXu2000@126.com>
2021-11-02 15:12:37 +0800

    modify bench mark and add logs

--- apps/VertexNormal/benchmark.sh
+++ apps/VertexNormal/benchmark.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "This script re-generates RXMesh data in Figure 8(d) in the paper."
 echo "Please make sure to first compile the source code and then enter the input OBJ files directory."
-read -p "OBJ files directory (no trailing slash): " input_dir
+#read -p "OBJ files directory (no trailing slash): " input_dir
 
-echo "Input directory= $input_dir"
+#echo "Input directory= $input_dir"
+echo "Input directory= $1"
 exe="../../build/bin/VertexNormal"
 
 if [ ! -f $exe ]; then 
@@ -14,7 +15,8 @@ fi
 num_run=10
 device_id=0
 
-for file in $input_dir/*.obj; do 	 
+#for file in $input_dir/*.obj; do 	 
+for file in $1/*.obj; do 	 
     if [ -f "$file" ]; then
 		echo $exe -p -input "$file" -num_run $num_run -device_id $device_id
              $exe -p -input "$file" -num_run $num_run -device_id $device_id
